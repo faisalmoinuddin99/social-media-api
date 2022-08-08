c9b22eb17a8243f69d1025aae3175cb601576ea6
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-08-08 05:58:43 +0530

    Path Variable GET method

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfullwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello world") ;
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWordPathVariable(@PathVariable String name){
+        return  new HelloWorldBean(String.format("Hello World, %s",name)) ;
     }
 }
